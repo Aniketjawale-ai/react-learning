a8013ab5dbe91ec1f014c2137b25b22c5b8f6ba5
Aniket <jawleaniket7@gmail.com>
2026-08-22 21:01:31 +0530

    Start React learning

---
+++ frist-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Title from "./Title.jsx";
+
+function Description() {
+  return <h3>I am Description!</h3>
+}
+function App(){
+  return (
+    <div>
+    <Title/>
+    <Description/>
+    <Title/>
+    <Description/>
+    </div>
+  );
+}
+export default App;

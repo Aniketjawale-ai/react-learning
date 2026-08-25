e7f9faece78a5abf166e8c8cce3963d46b2fcfde
Aniket <jawleaniket7@gmail.com>
2026-08-25 13:12:14 +0530

    Learn and implement React props

--- frist-react-app/src/ProductTab.jsx
+++ frist-react-app/src/ProductTab.jsx
@@ -1,11 +1,11 @@
 import Product  from "./Product.jsx";
 
 function ProductTab(){
+    let options = ["hi-tech", "durable", "fast"];
+    // let options2 = { a: "hi-teach", b: "durable", c:"fast"};
     return (
         <>
-        <Product/>
-        <Product/>
-        <Product/>
+    <Product title = "phone" price ={40000} features ={{a: "hi-tech"}} />
        </>
     );
 }
